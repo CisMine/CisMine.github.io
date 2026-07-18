c87177018a1bf5a98dfce5672a1528805dbed414
Gia Huy <ngogiahuy20012004@gmail.com>
2026-07-18 17:04:02 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahxt.github.io/academic-homepage-typst',
-  base: '/academic-homepage-typst/',
+  site: 'https://cismine.github.io/',
+  base: '/',
   
   integrations: [
     sitemap({
